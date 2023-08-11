6a58459e1166c58fe244c093bd6fbc64cc535752
Marilu34 <lulima@yandex.ru>
2023-08-11 22:18:18 +0300

    1

--- src/main/java/ru/practicum/shareit/comment/model/Comment.java
+++ src/main/java/ru/practicum/shareit/comment/model/Comment.java
@@ -13,12 +13,15 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments",
-        uniqueConstraints = {@UniqueConstraint(name = "ONE_USER_ONE_COMMENT", columnNames = {"item_id", "author_id"})})
+        uniqueConstraints = {
+        @UniqueConstraint(name = "ONE_USER_ONE_COMMENT",
+                columnNames = {"item_id", "author_id"})})
 @Getter
 @Setter
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
